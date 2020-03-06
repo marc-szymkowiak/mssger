12ed4b8b3e6f1c71a88565df4e7a19f32116a52d
szymkowiakm <szymkowiakm@WKC-ENT-001.wanko.local>
2020-03-06 13:35:16 +0100

    #

--- mssgER/src/mssgER/MssgServer.java
+++ mssgER/src/mssgER/MssgServer.java
@@ -1,7 +1,11 @@
 package mssgER;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MssgServer {
 	
@@ -15,4 +19,39 @@ public class MssgServer {
 		
 	}
 	
+	private void connectServer() {
+		while (true) {
+			Socket socket = null;
+			try {
+				socket = mssgServer.accept();
+				interconnect(socket);
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+				
+			}
+			finally {
+				if (socket != null)
+					try {
+						socket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+			}
+		}
+	} 
+	
+	private void interconnect(Socket socket) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket
+				.getInputStream()));
+		PrintStream out = new PrintStream(socket.getOutputStream());
+		String s;
+		
+		while(in.ready()) {
+			
+		}
+	}
+
+	
 }
+
